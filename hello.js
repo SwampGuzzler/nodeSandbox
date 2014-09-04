81ba3bb20072e7fe1e85e36cc86a10be3d4d0b9a
Lucas Cotner <lc07@uw.edu>
2014-09-04 13:26:02 -0700

    Finishing node chapter

--- hello.js
+++ hello.js
@@ -2,8 +2,11 @@ var http = require('http'); // Require in the http module
 
 http.createServer(function(request, response) {
 	response.writeHead(200); // Status code into the header
-	response.write("Hello, Node!"); 
-	response.end(); // Closes the connection
+	response.write("Hello, Node! "); 
+	setTimeout(function() {
+		response.write("Node is Done."),
+		response.end();
+	}, 5000);
 }).listen(8080); // listen on port 8080
 
 console.log("the server is running on port 8080");
